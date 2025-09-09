4fe832593ddc74e128a96727948aff39c545a0b2
Khushali1810 <vaghanikhushali59@gmail.com>
2025-09-09 10:55:04 +0530

    Add files via upload

---
+++ array .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a[5];
+    for(int i=0;i<5;i=i+1){
+        scanf("%d",&a[i]);
+    }
+
+}
+
---
+++ arrey 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a[10];
+    for(int i=0;i<10;i=i+1){
+        scanf("%d",&a[i]);
+        }
+        printf("%d %d %d",a[4],a[7],a[9]);
+
+}
+
